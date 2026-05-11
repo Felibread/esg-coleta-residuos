9d904456e23814bd567b9b9f9f91d4f62edba30f
Felipe Damasceno <patcho.olec@gmail.com>
2026-05-11 20:13:44 -0300

    Continuing project

--- src/test/java/com/esg/coleta/bdd/CucumberRunner.java
+++ src/test/java/com/esg/coleta/bdd/CucumberRunner.java
@@ -10,7 +10,7 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.esg.coleta.bdd.steps")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.esg.coleta.bdd")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
         value = "pretty, html:target/cucumber-reports/report.html, json:target/cucumber-reports/report.json")
 @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @skip")
